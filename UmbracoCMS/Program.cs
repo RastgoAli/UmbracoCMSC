5540e1ac26410cfe22aa9d11d4a1f9c586fca2ec
RastgoAli <rastgo_97@hotmail.com>
2025-11-16 21:29:34 +0100

    first commit

---
+++ UmbracoCMS/Program.cs
@@ -0,0 +1,32 @@
+using Umbraco.Cms.Web.Common.PublishedModels;
+using UmbracoCMS.Services;
+
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+builder.CreateUmbracoBuilder()
+    .AddBackOffice()
+    .AddWebsite()
+    .AddComposers()
+    .Build();
+
+builder.Services.AddScoped<FormSubmissionsService>();
+
+WebApplication app = builder.Build();
+
+await app.BootUmbracoAsync();
+
+app.UseHttpsRedirection();
+
+app.UseUmbraco()
+    .WithMiddleware(u =>
+    {
+        u.UseBackOffice();
+        u.UseWebsite();
+    })
+    .WithEndpoints(u =>
+    {
+        u.UseBackOfficeEndpoints();
+        u.UseWebsiteEndpoints();
+    });
+
+await app.RunAsync();
---
+++ UmbracoCMS/ViewModels/CallbackFormViewModel.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System.ComponentModel.DataAnnotations;
+
+namespace UmbracoCMS.ViewModels;
+
+public class CallbackFormViewModel
+{
+    [Required(ErrorMessage = "Name is required")]
+    [Display(Name = "Name")]
+    public string Name { get; set; } = null!;
+
+    [Required(ErrorMessage = "Email is required")]
+    [Display(Name = "Email address")]
+    [RegularExpression(@"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$", ErrorMessage = "Ogiltig e-postadress.")]
+    public string Email { get; set; } = null!;
+
+    [Required(ErrorMessage = "Phone Number is required")]
+    [Display(Name = "Phone")]
+    public string Phone { get; set; } = null!;
+
+    [Required(ErrorMessage = "Select an option")]
+    public string SelectedOption { get; set; } = null!;
+
+    [BindNever]
+    public IEnumerable<string> Options { get; set; } = [];
+}
